56d94325c1cb88f2d43ee727b674d6c5f5516f93
mauroarc92 <mauricioarcila53@gmail.com>
2021-06-10 20:52:44 -0500

    creacion de componentes y navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { GotyComponent } from './pages/goty/goty.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GraficaComponent } from './components/grafica/grafica.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    GotyComponent,
+    NavbarComponent,
+    GraficaComponent
   ],
   imports: [
     BrowserModule,
